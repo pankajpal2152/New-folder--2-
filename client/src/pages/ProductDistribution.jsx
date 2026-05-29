f67f038a3329788cf73789c838a8a27049cc1e60
Pankaj Pal <pankajpal8967890080@gmail.com>
2026-05-29 12:03:43 +0530

    nexttt

--- client/src/pages/ProductDistribution.jsx
+++ client/src/pages/ProductDistribution.jsx
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from "react";
-// import "../../styles/Productdistibution.css";
-// import "../../styles/login.css";
+import "./Productdistibution.css"; 
 import axios from "axios";
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import { FaEdit, FaTrash, FaEye } from "react-icons/fa";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FaEdit, FaTrash, FaEye } from "react-icons/fa";
 export default function Productdistibution() {
   // ====================================
   // STATE
